b06752189559cf760e8f232603da7fc4302a61da
wahyudedik <wdedyk@gmail.com>
2026-05-29 19:47:23 +0700

    Fsdfsdf

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -78,7 +78,7 @@
         <div class="container mx-auto px-4 sm:px-6 lg:px-8">
             <div class="text-center mb-12">
                 <h2 class="text-2xl font-bold text-gray-800 uppercase tracking-wide">RUANG TULISAN</h2>
-                <p class="text-gray-600 mt-2">Kumpulan artikel dan berita terkini dari Pena Langit</p>
+                <p class="text-gray-600 mt-2">Kumpulan artikel dan berita terkini dari CV. Azriya permata Group</p>
             </div>
 
             <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
